05a399412c365f5d1e90e4203d54289dbd76f0ab
Dataram57 <dataram66657@gmail.com>
2023-05-17 15:12:01 +0200

    some stuff

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const InitPGPToolPage = () => {
+};
+
+const ShineParent = (element) => {
+    element = element.parentElement;
+    //repeat animation
+    element.classList.remove('fx-shine1');
+    element.classList.add('fx-shine1');
+    //https://stackoverflow.com/questions/6268508/restart-animation-in-css3-any-better-way-than-removing-the-element
+    element.style.animation = 'none';
+    element.offsetHeight; /* trigger reflow */
+    element.style.animation = null; 
+};
+
+const ClickGenerate = async () => {
+    //get result
+    const result = await GenerateRSAPGP(Password.value, 2048 * 2);
+    //apply
+    PublicKey.value = result.publicKey;
+    PrivateKey.value = result.privateKey; 
+    //shine
+    ShineParent(Password);
+    ShineParent(PublicKey);
+    ShineParent(PrivateKey);
+};
+
+const ClickEncrypt = async () => {
+
+};
+
 const GenerateRSAPGP = async (password,rsaBits) => {
     const { privateKey, publicKey } = await openpgp.generateKey({
         type: 'rsa', // Type of the key
@@ -5,7 +35,6 @@ const GenerateRSAPGP = async (password,rsaBits) => {
         userIDs: [{ name: 'Jon Smith', email: 'jon@example.com' }], // you can pass multiple user IDs
         passphrase: password // protects the private key
     })
-
     return {
         publicKey: publicKey
         ,privateKey: privateKey
